6048527dfd61ac278b1ac4638226dc1499d26f48
Benjamin Pablo <benjaminpablo2345@gmail.com>
2022-06-21 14:10:52 -0500

    Reestructuring the position of the btn

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,4 +1,4 @@
-const btnEl = document.querySelector('.btn');
+const btnNewEl = document.querySelector('.btn--new-task');
 
 // ✨ Features:
 // 1. Be able to add new elements to a list
@@ -12,11 +12,12 @@ const btnEl = document.querySelector('.btn');
 
 // b. Divide and conquer: Break a big problem into smaller sub-problems (task list).
 // 1.1. Add a event listener to the btnEl.
-btnEl.addEventListener('click', () => {
+btnNewEl.addEventListener('click', () => {
+  // 1.2. When the btn is clicked we render a modal window and fill the description and category.
+
   console.log('Added the event listener');
 });
 
-// 1.2. When the btn is clicked we render a modal window and fill the description and category.
 // 1.3. Store the values on variables.
 // 1.4. Then we clicked the button add and we render the new tasks__item at the top of the tasks__list.
 
