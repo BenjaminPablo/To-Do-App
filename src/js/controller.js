4c552db6bd1dad3633b8f97772a200353f3f7068
Benjamin Pablo <benjaminpablo2345@gmail.com>
2022-06-21 11:14:45 -0500

    Working on the js

---
+++ src/js/controller.js
@@ -0,0 +1,24 @@
+const btnEl = document.querySelector('.btn');
+
+// ✨ Features:
+// 1. Be able to add new elements to a list
+// 2. Be able to remove existing elements from a list
+// 3. Be able to rename existing elements in a list
+// 4. Be able to see the number of complete and incomplete elements
+
+// Solving Problem Framework:
+// a. Make sure you 100% understand the problem. Ask the right questions to get a clear picture of the problem.
+// 1. Be able to add new elements to a list
+
+// b. Divide and conquer: Break a big problem into smaller sub-problems (task list).
+// 1.1. Add a event listener to the btnEl.
+btnEl.addEventListener('click', () => {
+  console.log('Added the event listener');
+});
+
+// 1.2. When the btn is clicked we render a modal window and fill the description and category.
+// 1.3. Store the values on variables.
+// 1.4. Then we clicked the button add and we render the new tasks__item at the top of the tasks__list.
+
+// c. Only if you can't figure out the solution in your own, do as much research as you have to (google, stack overflow, mdn, etc).
+// d. For bigger problems, write pseudo-code before writing the actual code.
