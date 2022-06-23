7e0dcd4ecb61ffb68bd92f47f50b1a56b1371b6e
Benjamin Pablo <benjaminpablo2345@gmail.com>
2022-06-23 16:40:44 -0500

    Reestructuring the dialog

--- src/js/controller.js
+++ src/js/controller.js
@@ -9,8 +9,18 @@ const tasksListIncompleteEl = document.querySelector(
   '.tasks__list--incomplete'
 );
 const btnAddNewTaskEl = document.querySelector('.btn--new-task');
-
 let previousActiveEl = dialogTextDesEl.focus();
+
+const openDialog = () => {
+  // To store the previousActiveEl
+  // previousActiveEl = document.activeElement;
+  // Array.from(document.body.children).forEach(child => {
+  // if (child !== dialogEl) child.inert = true;
+  // });
+  dialogOverlayEl.classList.remove('hidden');
+  dialogGroupEl.classList.remove('hidden');
+};
+btnOpenEl.addEventListener('click', openDialog);
 
 // ✨ Features:
 // 1. Be able to add new elements to a list
@@ -24,65 +34,65 @@ let previousActiveEl = dialogTextDesEl.focus();
 // 1. Be able to add new elements to a list
 // 1.1. Open and closing the dialog with the btn.
 
-const dialog = () => {
-  const openDialog = () => {
-    // To store the previousActiveEl
-    previousActiveEl = document.activeElement;
-    Array.from(document.body.children).forEach(child => {
-      if (child !== dialogEl) child.inert = true;
-    });
+// const dialog = () => {
+//   const openDialog = () => {
+//     // To store the previousActiveEl
+//     previousActiveEl = document.activeElement;
+//     Array.from(document.body.children).forEach(child => {
+//       if (child !== dialogEl) child.inert = true;
+//     });
 
-    dialogOverlayEl.classList.remove('hidden');
-    dialogGroupEl.classList.remove('hidden');
-  };
+//     dialogOverlayEl.classList.remove('hidden');
+//     dialogGroupEl.classList.remove('hidden');
+//   };
 
-  const closeDialog = () => {
-    dialogOverlayEl.classList.add('hidden');
-    dialogGroupEl.classList.add('hidden');
-    Array.from(document.body.children).forEach(child => {
-      if (child !== dialogEl) child.inert = false;
-    });
+//   const closeDialog = () => {
+//     dialogOverlayEl.classList.add('hidden');
+//     dialogGroupEl.classList.add('hidden');
+//     Array.from(document.body.children).forEach(child => {
+//       if (child !== dialogEl) child.inert = false;
+//     });
 
-    previousActiveEl.focus();
-  };
+//     previousActiveEl.focus();
+//   };
 
-  btnOpenEl.addEventListener('click', openDialog);
-  btnCloseEl.addEventListener('click', closeDialog);
-  dialogOverlayEl.addEventListener('click', closeDialog);
-  document.addEventListener('keydown', e =>
-    e.key === 'Escape' && !dialogGroupEl.classList.contains('hidden')
-      ? closeDialog()
-      : ''
-  );
-};
-dialog();
+//   btnOpenEl.addEventListener('click', openDialog);
+//   btnCloseEl.addEventListener('click', closeDialog);
+//   dialogOverlayEl.addEventListener('click', closeDialog);
+//   document.addEventListener('keydown', e =>
+//     e.key === 'Escape' && !dialogGroupEl.classList.contains('hidden')
+//       ? closeDialog()
+//       : ''
+//   );
+// };
+// dialog();
 
-// 1.2 Render the new task to the task list
-const renderTasks = () => {
-  const html = `
-    <div class="tasks__item">
-      <input
-        class="tasks__checkbox"
-        type="checkbox"
-        id="${dialogTextDesEl.value}"
-      />
-      <div class="tasks__description">
-        <label class="tasks__label" for="${dialogTextDesEl.value}">${dialogTextDesEl.value}</label>
-        <p class="tasks__category">${dialogTextCatEl.value}</p>
-      </div>
-    </div>
-  `;
-  tasksListIncompleteEl.insertAdjacentHTML('afterbegin', html);
-  dialogOverlayEl.classList.add('hidden');
-  dialogGroupEl.classList.add('hidden');
-  Array.from(document.body.children).forEach(child => {
-    if (child !== dialogEl) child.inert = false;
-  });
-  dialogTextDesEl.value = '';
-  dialogTextCatEl.value = '';
-  dialogTextDesEl.focus();
-};
-btnAddNewTaskEl.addEventListener('click', renderTasks);
+// // 1.2 Render the new task to the task list
+// const renderTasks = () => {
+//   const html = `
+//     <div class="tasks__item">
+//       <input
+//         class="tasks__checkbox"
+//         type="checkbox"
+//         id="${dialogTextDesEl.value}"
+//       />
+//       <div class="tasks__description">
+//         <label class="tasks__label" for="${dialogTextDesEl.value}">${dialogTextDesEl.value}</label>
+//         <p class="tasks__category">${dialogTextCatEl.value}</p>
+//       </div>
+//     </div>
+//   `;
+//   tasksListIncompleteEl.insertAdjacentHTML('afterbegin', html);
+//   dialogOverlayEl.classList.add('hidden');
+//   dialogGroupEl.classList.add('hidden');
+//   Array.from(document.body.children).forEach(child => {
+//     if (child !== dialogEl) child.inert = false;
+//   });
+//   dialogTextDesEl.value = '';
+//   dialogTextCatEl.value = '';
+//   dialogTextDesEl.focus();
+// };
+// btnAddNewTaskEl.addEventListener('click', renderTasks);
 
 // 1.3. Store the values on variables.
 // 1.4. Then we clicked the button add and we render the new tasks__item at the top of the tasks__list.
