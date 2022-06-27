a5f283f412c5cd8d8871f8ddd457350a0e07226a
Benjamin Pablo <benjaminpablo2345@gmail.com>
2022-06-27 12:05:46 -0500

    Main dialog completed, working on small dialog

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,12 +1,12 @@
 const btnOpenEl = document.querySelector('.btn--open');
 const btnCloseEl = document.querySelector('.btn--close');
-const dialogEl = document.querySelector('.dialog');
+const dialogAddNewTaskEl = document.querySelector('.dialog--new-task');
 const dialogTextDesEl = document.querySelector('.dialog__text--description');
 const dialogTextCatEl = document.querySelector('.dialog__text--category');
 const tasksListIncompleteEl = document.querySelector(
   '.tasks__list--incomplete'
 );
-const btnAddNewTaskEl = document.querySelector('.btn--new-task');
+const btnPrincipalEl = document.querySelector('.btn--principal');
 
 // ✨ Features:
 // 1. Be able to add new elements to a list
@@ -19,61 +19,50 @@ const btnAddNewTaskEl = document.querySelector('.btn--new-task');
 // b. Divide and conquer: Break a big problem into smaller sub-problems (task list).
 // 1. Be able to add new elements to a list
 // 1.1. Open and closing the dialog with the btn.
+const cleanInputFields = () => {
+  dialogTextDesEl.value = '';
+  dialogTextCatEl.value = '';
+};
 
 const dialog = () => {
-  const openDialog = () => {
-    dialogEl.showModal();
-    dialogTextDesEl.focus();
-    dialogEl.classList.add('open');
-  };
-
-  const closeDialog = () => {
-    dialogEl.classList.remove('open');
-    dialogEl.addEventListener('transitionend', killTransitionDialog);
-  };
-  const killTransitionDialog = () => {
-    dialogEl.close();
-    dialogEl.removeEventListener('transitionend', killTransitionDialog);
-  };
-
-  btnOpenEl.addEventListener('click', openDialog);
-  btnCloseEl.addEventListener('click', closeDialog);
-  dialogEl.addEventListener('click', e => {
-    const dialogRect = dialogEl.getBoundingClientRect();
-    const checkClickInsideDialog = e.clientX >= dialogRect.left;
+  btnOpenEl.addEventListener('click', () => {
+    dialogAddNewTaskEl.showModal();
+    cleanInputFields();
   });
-  document.addEventListener('keydown', e => {
-    if (e.key === 'Escape') closeDialog();
+  btnCloseEl.addEventListener('click', () => {
+    dialogAddNewTaskEl.close();
+  });
+  dialogAddNewTaskEl.addEventListener('click', e => {
+    const dialogRect = dialogAddNewTaskEl.getBoundingClientRect();
+    const checkClickInsideDialog =
+      e.clientY >= dialogRect.top &&
+      e.clientY <= dialogRect.top + dialogRect.height &&
+      e.clientX >= dialogRect.left &&
+      e.clientX <= dialogRect.left + dialogRect.width;
+    if (!checkClickInsideDialog) dialogAddNewTaskEl.close();
   });
 };
 dialog();
 
-// // 1.2 Render the new task to the task list
-// const renderTasks = () => {
-//   const html = `
-//     <div class="tasks__item">
-//       <input
-//         class="tasks__checkbox"
-//         type="checkbox"
-//         id="${dialogTextDesEl.value}"
-//       />
-//       <div class="tasks__description">
-//         <label class="tasks__label" for="${dialogTextDesEl.value}">${dialogTextDesEl.value}</label>
-//         <p class="tasks__category">${dialogTextCatEl.value}</p>
-//       </div>
-//     </div>
-//   `;
-//   tasksListIncompleteEl.insertAdjacentHTML('afterbegin', html);
-//   dialogOverlayEl.classList.add('hidden');
-//   dialogGroupEl.classList.add('hidden');
-//   Array.from(document.body.children).forEach(child => {
-//     if (child !== dialogEl) child.inert = false;
-//   });
-//   dialogTextDesEl.value = '';
-//   dialogTextCatEl.value = '';
-//   dialogTextDesEl.focus();
-// };
-// btnAddNewTaskEl.addEventListener('click', renderTasks);
+// 1.2 Render the new task to the task list
+const renderTask = () => {
+  const html = `
+    <div class="tasks__item">
+      <input
+        class="tasks__checkbox"
+        type="checkbox"
+        id="${dialogTextDesEl.value}"
+      />
+      <div class="tasks__description">
+        <label class="tasks__label" for="${dialogTextDesEl.value}">${dialogTextDesEl.value}</label>
+        <p class="tasks__category">${dialogTextCatEl.value}</p>
+      </div>
+    </div>
+  `;
+  tasksListIncompleteEl.insertAdjacentHTML('afterbegin', html);
+  dialogAddNewTaskEl.close();
+};
+btnPrincipalEl.addEventListener('click', renderTask);
 
 // 1.3. Store the values on variables.
 // 1.4. Then we clicked the button add and we render the new tasks__item at the top of the tasks__list.
