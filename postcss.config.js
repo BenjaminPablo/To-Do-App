ba952966f4c03b989f4234c184dd2636abda1ca6
Benjamin Pablo <benjaminpablo2345@gmail.com>
2022-07-08 18:00:29 -0500

    Adding new postcss plugins

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,15 @@
 // postcss.config.js
 const postcssJitProps = require('postcss-jit-props');
 const OpenProps = require('open-props');
+const postcssCustomMedia = require('postcss-custom-media');
+const postcssPresetEnv = require('postcss-preset-env');
+const postcssAutoPrefixer = require('autoprefixer');
 
 module.exports = {
-  plugins: [postcssJitProps(OpenProps)],
+  plugins: [
+    postcssJitProps(OpenProps),
+    postcssCustomMedia(/* pluginOptions */),
+    postcssPresetEnv(/* pluginOptions */),
+    postcssAutoPrefixer(),
+  ],
 };
