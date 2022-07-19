6eb9c53f9130c9ac56a712960fded0a54378a56a
Benjamin Pablo <benjaminpablo2345@gmail.com>
2022-07-18 19:56:13 -0500

    Adding OOP to the project

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,9 +1,6 @@
 import icons from '../img/svg/sprite.svg';
 
 // Header component
-const headerStatusChildrenEl = Array.from(
-  document.querySelector('.header__status').children
-);
 const headerCounterIncompletedEl = document.querySelector(
   '.header__incompleted'
 );
@@ -21,24 +18,71 @@ const formSelectEl = document.querySelector('.form__select');
 class App {
   // We set the text content to both header status children
   #counterTasksIncompleted = 5;
-  #counterTasksCompleted = 5;
+  // #counterTasksCompleted = 5;
+  #randomID;
+  #markup;
 
   constructor() {
-    // Counters for header__status
-    headerStatusChildrenEl.forEach(
-      headerStatus => (headerStatus.textContent = 5)
-    );
-
+    // ✅ ✨ Features:
+    // ✅ 1. Be able to add new elements to a list
+    this.#randomID = Math.floor(Math.random() * 10000);
     btnRenderTaskFormEl.addEventListener('click', this.#renderNewTaskForm);
+    btnSubmitEl.addEventListener('click', this.#renderNewTask.bind(this));
+    formEl.addEventListener('input', this.#valInputsTaskForm);
+    document.addEventListener('keydown', this.#valKeyTaskForm.bind(this));
+    formEl.addEventListener('focusout', this.#closeFormOnClick);
+    btnCancelEl.addEventListener('click', this.#showBtnRenderTaskForm);
   }
 
   #renderNewTaskForm() {
+    formTextEl.value = formSelectEl.value = '';
+    btnSubmitEl.setAttribute('inert', '');
+    btnSubmitEl.classList.add('u-opacity-0-5');
     formEl.classList.remove('hidden');
-    btnRenderTaskFormEl.classList.add('hidden');
+    formEl.classList.remove('u-visibility-hidden');
+    this.classList.add('hidden');
     formTextEl.focus();
   }
 
-  #renderNewTask() {}
+  #renderNewTask(e) {
+    e.preventDefault();
+    // Formatting the values and adding the randomid in the html
+    this.#markup = `
+    <fieldset class="tasks__item" data-status="incompleted" tabindex="0">
+      <input
+        class="tasks__checkbox"
+        type="checkbox"
+        aria-label="checkbox"
+        id="${this.#randomID}"
+      />
+      <label class="tasks__label">
+        <span class="tasks__description">${
+          formTextEl.value[0].toUpperCase() +
+          formTextEl.value.slice(1).toLowerCase()
+        }</span>
+        <span class="tasks__category">${formSelectEl.value}</span>
+      </label>
+      <button
+        class="btn btn--delete"
+        aria-label="Remove task item"
+        title="Remove task item"
+      >
+        <svg class="btn__icon btn__icon--delete-task">
+          <use href="src/img/svg/sprite.svg#icon-trashcan"></use>
+        </svg>
+      </button>
+    </fieldset>`;
+
+    // When adding a new item, the score of the one belonging to the list increases.
+    this.#counterTasksIncompleted++;
+    headerCounterIncompletedEl.textContent = this.#counterTasksIncompleted;
+    // Inserting the html
+    tasksHeadingEl.insertAdjacentHTML('afterend', this.#markup);
+    formEl.classList.add('hidden');
+    formEl.classList.add('u-visibility-hidden');
+    btnRenderTaskFormEl.classList.remove('hidden');
+    btnRenderTaskFormEl.focus();
+  }
 
   #showOptionsTask() {}
 
@@ -61,155 +105,88 @@ class App {
   #showOptionsList() {}
 
   #selectMultipleTasks() {}
+
+  // Methods that serve to the main events
+  // Task 1
+  #valInputsTaskForm() {
+    if (formSelectEl.value === '' || formTextEl.value === '') {
+      btnSubmitEl.setAttribute('inert', '');
+      btnSubmitEl.classList.add('u-opacity-0-5');
+    } else {
+      btnSubmitEl.removeAttribute('inert', '');
+      btnSubmitEl.classList.remove('u-opacity-0-5');
+    }
+  }
+
+  #valKeyTaskForm(e) {
+    if (e.key === 'Escape') {
+      // Checking if the user presses the Esc key in any time once the tasksForm is shown
+      formEl.classList.add('hidden');
+      formEl.classList.add('u-visibility-hidden');
+      btnRenderTaskFormEl.classList.remove('hidden');
+      btnRenderTaskFormEl.focus();
+    }
+    if (
+      // If the form text is empty, then we stop the submit
+      e.key === 'Enter' &&
+      formTextEl.value === '' &&
+      formTextEl === document.activeElement
+    ) {
+      e.preventDefault();
+    }
+    if (
+      // If the user didn't choose any category, then we prevent the submit
+      e.key === 'Enter' &&
+      formSelectEl.value === '' &&
+      formSelectEl !== document.activeElement &&
+      btnCancelEl !== document.activeElement &&
+      btnRenderTaskFormEl !== document.activeElement
+    ) {
+      e.preventDefault();
+    }
+    if (
+      // If the text and select have been completed, then we add the new task to the incompleted list.
+      e.key === 'Enter' &&
+      formTextEl.value !== '' &&
+      formSelectEl.value !== '' &&
+      btnCancelEl !== document.activeElement &&
+      btnRenderTaskFormEl !== document.activeElement
+    ) {
+      this.#renderNewTask(e);
+      btnRenderTaskFormEl.focus();
+    }
+  }
+
+  #closeFormOnClick(e) {
+    if (e.relatedTarget !== null) return;
+    formEl.classList.add('hidden');
+    formEl.classList.add('u-visibility-hidden');
+    btnRenderTaskFormEl.classList.remove('hidden');
+    btnRenderTaskFormEl.focus();
+  }
+
+  #showBtnRenderTaskForm(e) {
+    e.preventDefault();
+    formEl.classList.add('u-visibility-hidden');
+    formEl.classList.add('hidden');
+    btnRenderTaskFormEl.classList.remove('hidden');
+    btnRenderTaskFormEl.focus();
+  }
 }
 
 const app = new App();
 
-// ✅ ✨ Features:
-// ✅ 1. Be able to add new elements to a list
-// 1.1. After finishing load
-// showBtnRenderTaskForm();
-// 1.2. Render task form
-
-// 1.3 Validation and submit for keydown, only validating for the input event
-// document.addEventListener('keydown', validateKeyTaskForm);
-// formEl.addEventListener('input', activateBtnSubmit);
-// // 1.4. Adding a new task
-// btnSubmitEl.addEventListener('click', addNewTask);
-// // Canceling the submit and replacing the form
-// formEl.addEventListener('focusout', closeFormOnClick);
-// btnCancelEl.addEventListener('click', showBtnRenderTaskForm);
+// document.addEventListener('keydown', () => {
+//   console.log(document.activeElement);
+// });
 
 // const p = document.createElement('p');
 // const pCompleted = document.createElement('p');
 // p.textContent = `You don't have any tasks for the moment to complete! 🎉`;
 // pCompleted.textContent = `Your completed tasks are empty! 🎉`;
 
-// Task 1 - Functions
-// To disable the btnSubmit
-const disableBtnSubmit = () => {
-  btnSubmitEl.setAttribute('inert', '');
-  btnSubmitEl.classList.add('u-opacity-0-5');
-};
-
-// To enable the btnSubmit
-const enableBtnSubmit = () => {
-  btnSubmitEl.removeAttribute('inert');
-  btnSubmitEl.classList.remove('u-opacity-0-5');
-};
-
-const renderTaskForm = () => {};
-
-const showBtnRenderTaskForm = e => {
-  btnRenderTaskFormEl.focus();
-  formEl.removeEventListener('focusout', closeFormOnClick);
-  formEl.replaceWith(btnRenderTaskFormEl);
-  restartForm();
-  // if (!tasksHeadingEl.contains(btnRenderTaskFormEl)) {
-  //   e.preventDefault();
-  // }
-  // Removes the focusevent so we don't have two events active, only one, focusout or click
-};
-
-const cleanInputValues = () => {
-  formTextEl.value = '';
-  formSelectEl.value = '';
-};
-
-const restartForm = () => {
-  cleanInputValues();
-  disableBtnSubmit();
-};
-
-const validateKeyTaskForm = e => {
-  if (e.key === 'Escape') {
-    // Checking if the user presses the Esc key in any time once the tasksForm is shown
-    showBtnRenderTaskForm(e);
-  }
-  if (
-    // If the form text is empty, then we stop the submit
-    e.key === 'Enter' &&
-    formTextEl.value === '' &&
-    formTextEl === document.activeElement
-  ) {
-    e.preventDefault();
-  }
-  if (
-    // If the user didn't choose any category, then we prevent the submit
-    e.key === 'Enter' &&
-    formSelectEl.value === '' &&
-    formSelectEl !== document.activeElement &&
-    btnCancelEl !== document.activeElement &&
-    btnRenderTaskFormEl !== document.activeElement
-  ) {
-    e.preventDefault();
-  }
-  if (
-    // If the text and select have been completed, then we add the new task to the incompleted list.
-    e.key === 'Enter' &&
-    formTextEl.value !== '' &&
-    formSelectEl.value !== '' &&
-    btnCancelEl !== document.activeElement
-  ) {
-    addNewTask(e);
-    btnRenderTaskFormEl.focus();
-  }
-};
-
-const formatInputValue = formEl =>
-  formEl.value[0].toUpperCase() + formEl.value.slice(1).toLowerCase();
-
-// To add a new task item to the incompleted tasks list
-const addNewTask = e => {
-  formEl.addEventListener('focusout', closeFormOnClick);
-  // Formatting the values and adding the randomid in the html
-  const randomID = Math.floor(Math.random() * 10000);
-  const markup = `
-    <fieldset class="tasks__item" data-status="incompleted" tabindex="0">
-      <input
-        class="tasks__checkbox"
-        type="checkbox"
-        aria-label="checkbox"
-        id="${randomID}"
-      />
-      <label class="tasks__label">
-        <span class="tasks__description">${formatInputValue(formTextEl)}</span>
-        <span class="tasks__category">${formSelectEl.value}</span>
-      </label>
-      <button
-        class="btn btn--delete"
-        aria-label="Remove task item"
-        title="Remove task item"
-      >
-        <svg class="btn__icon btn__icon--delete-task">
-          <use href="src/img/svg/sprite.svg#icon-trashcan"></use>
-        </svg>
-      </button>
-    </fieldset>`;
-
-  // When adding a new item, the score of the one belonging to the list increases.
-  counterTasksIncompleted++;
-  headerCounterIncompletedEl.textContent = counterTasksIncompleted;
-  // Inserting the html
-  tasksHeadingEl.insertAdjacentHTML('afterend', markup);
-  btnRenderTaskFormEl.focus();
-  showBtnRenderTaskForm(e);
-};
-
-const activateBtnSubmit = () => {
-  formSelectEl.value === '' || formTextEl.value === ''
-    ? disableBtnSubmit()
-    : enableBtnSubmit();
-};
-
-const closeFormOnClick = function (e) {
-  if (e.relatedTarget !== null) return;
-  this.replaceWith(btnRenderTaskFormEl);
-  restartForm();
-};
-
-// // ✅ 2. Be able to remove existing elements from a list
-// // 2.1 Add event handler to the delete button to show the dialog of confirmation to delete the task.
+// ✅ 2. Be able to remove existing elements from a list
+// 2.1 Add event handler to the delete button to show the dialog of confirmation to delete the task.
 // const deleteTask = () => {
 //   sectionTasksEl.addEventListener('click', e => {
 //     const btnDelete = e.target.closest('.btn--delete');
@@ -248,51 +225,51 @@ const closeFormOnClick = function (e) {
 // };
 // deleteTask();
 
-// // ✅ 3. Be able to rename existing elements in a list
+// ✅ 3. Be able to rename existing elements in a list
 // const renameTask = () => {
-//   // // tasksDescriptionEl.forEach(task => {
-//   //   // 3.1. Replace the label with a new input field by double clicking the label, of course, to edit it.
-//   //   tasksIncompletedEl.addEventListener('dblclick', e => {
-//   //     const taskDesChild = e.target.closest(`.${task.className}`);
-//   //     if (!taskDesChild) return;
-//   //     const newInput = document.createElement('input');
-//   //     const typeChild =
-//   //       task.className === 'tasks__label'
-//   //         ? 'input-text__update--description'
-//   //         : 'input-text__update--category';
-//   //     newInput.classList.add('input-text', typeChild);
-//   //     // We assign the value of the task label to the new input, and we also delete the spaces.
-//   //     newInput.value = taskDesChild.textContent.trim();
-//   //     // Then, we replace the task label with the new input whenever the user double clicks the label.
-//   //     taskDesChild.replaceWith(newInput);
-//   //     // newInput.setSelectionRange(0, 0);
-//   //     newInput.focus();
-//   //     const onFocus = function () {
-//   //       // We delete the listener and the replace with each time so we can get a new replacewith method working
-//   //       this.removeEventListener('blur', onFocus);
-//   //       // So, if we lose the focus on the input, we replace it with the label that was preceding it.
-//   //       this.replaceWith(taskDesChild);
-//   //     };
-//   //     newInput.addEventListener('blur', onFocus);
-//   //     newInput.addEventListener('keydown', function (e) {
-//   //       // If we just hit escape, we do replace the input with the label, but with any changes at all.
-//   //       if (e.key === 'Escape') {
-//   //         this.removeEventListener('blur', onFocus);
-//   //         this.replaceWith(taskDesChild);
-//   //       }
-//   //       // Meanwhile we hit enter and the value is not empty, we can store that value to the task and display it
-//   //       if (e.key === 'Enter' && this.value !== '') {
-//   //         this.removeEventListener('blur', onFocus);
-//   //         taskDesChild.textContent = this.value;
-//   //         this.replaceWith(taskDesChild);
-//   //       }
-//   //     });
-//   //   });
-//   // });
+// // tasksDescriptionEl.forEach(task => {
+//   // 3.1. Replace the label with a new input field by double clicking the label, of course, to edit it.
+//   tasksIncompletedEl.addEventListener('dblclick', e => {
+//     const taskDesChild = e.target.closest(`.${task.className}`);
+//     if (!taskDesChild) return;
+//     const newInput = document.createElement('input');
+//     const typeChild =
+//       task.className === 'tasks__label'
+//         ? 'input-text__update--description'
+//         : 'input-text__update--category';
+//     newInput.classList.add('input-text', typeChild);
+//     // We assign the value of the task label to the new input, and we also delete the spaces.
+//     newInput.value = taskDesChild.textContent.trim();
+//     // Then, we replace the task label with the new input whenever the user double clicks the label.
+//     taskDesChild.replaceWith(newInput);
+//     // newInput.setSelectionRange(0, 0);
+//     newInput.focus();
+//     const onFocus = function () {
+//       // We delete the listener and the replace with each time so we can get a new replacewith method working
+//       this.removeEventListener('blur', onFocus);
+//       // So, if we lose the focus on the input, we replace it with the label that was preceding it.
+//       this.replaceWith(taskDesChild);
+//     };
+//     newInput.addEventListener('blur', onFocus);
+//     newInput.addEventListener('keydown', function (e) {
+//       // If we just hit escape, we do replace the input with the label, but with any changes at all.
+//       if (e.key === 'Escape') {
+//         this.removeEventListener('blur', onFocus);
+//         this.replaceWith(taskDesChild);
+//       }
+//       // Meanwhile we hit enter and the value is not empty, we can store that value to the task and display it
+//       if (e.key === 'Enter' && this.value !== '') {
+//         this.removeEventListener('blur', onFocus);
+//         taskDesChild.textContent = this.value;
+//         this.replaceWith(taskDesChild);
+//       }
+//     });
+//   });
+// });
 // };
 // renameTask();
 
-// // ✅ 4. Be able to see the number of complete and incomplete elements
+// ✅ 4. Be able to see the number of complete and incomplete elements
 // const updateNumberTasks = () => {
 //   // 4.1. Add an event listener for all the incompleted tasks checkboxes
 //   tasksIncompletedEl.addEventListener('click', e => {
